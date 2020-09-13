dc7aa3766ef3ca464b7dfddd9177e4e4bc4a7957
matthew-devonport <roarsintheeast@gmail.com>
2020-09-13 14:50:10 +1200

    setup form on app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,6 +8,23 @@ function Todo({ todo, index }) {
   )
 }
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -21,12 +38,18 @@ function App() {
     },
   ]);
 
+  const addTodo = text => {
+    const NewTodos = [...todos, { text }];
+    setTodos(NewTodos);
+  }
+
   return (
     <div className="todoApp">
       <div className="todoList">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
